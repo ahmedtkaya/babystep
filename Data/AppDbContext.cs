f7650937640161716698cdf9474caac4d600b419
Ahmed tayyib Kaya <ahmedtkaya@Ahmed-MBP.hgw.local>
2025-02-19 14:50:38 +0300

    created2

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace babystepV1.Data
+{
+    public class AppDbContext
+    {
+        
+    }
+}
